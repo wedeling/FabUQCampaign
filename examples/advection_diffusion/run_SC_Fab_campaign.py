99bbbe433359b7cb70f55710712b4eb78de2960a
wouter <wouteredeling@gmail.com>
2019-04-03 13:31:22 +0200

    updated advection diffusion example with Chaospy use

--- examples/advection_diffusion/run_SC_Fab_campaign.py
+++ examples/advection_diffusion/run_SC_Fab_campaign.py
@@ -8,6 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import easyvvuq as uq
 import os
+import chaospy as cp
+from collections import OrderedDict
 
 # Input file containing information about parameters of interest
 input_json = "ade_input.json"
@@ -21,14 +23,12 @@ my_campaign = uq.Campaign(state_filename=input_json)
 #    distribution from which to draw samples
 #!NOTE: the variables are in the standard domain [-1, 1]. The mapping to
 #       their physical range is done in ADE.py
+m = 4 
+my_campaign.vary_param("Pe", dist=cp.distributions.Uniform(-1, 1))
+my_campaign.vary_param("f", dist=cp.distributions.Uniform(-1, 1))
 
-m1 = 6
-m2 = 6
-my_campaign.vary_param("Pe", dist=uq.distributions.legendre(m1))
-my_campaign.vary_param("f", dist=uq.distributions.legendre(m2))
-
-## 3. Select the SC sampler to create a tensor grid
-sc_sampler = uq.elements.sampling.SCSampler(my_campaign)
+# 3. Select the SC sampler to create a tensor grid
+sc_sampler = uq.elements.sampling.SCSampler(my_campaign, m)
 number_of_samples = sc_sampler.number_of_samples
 
 my_campaign.add_runs(sc_sampler, max_num=number_of_samples)
@@ -73,27 +73,27 @@ aggregate.apply()
 # 7.  Post-processing analysis: computes the 1st two statistical moments and
 #     gives the ability to use the SCAnalysis object as a surrogate, which
 #     interpolated the code samples to unobserved parameter variables.
-sc_analysis = uq.elements.analysis.SCAnalysis(my_campaign, value_cols=output_columns)
-results, output_file = sc_analysis.get_moments()    #moment calculation
-#results, output_file = sc_analysis.apply()
-#print(sc_analysis.get_Sobol_indices('first_order'))
+sc_analysis = uq.elements.analysis.SCAnalysis(
+    my_campaign, value_cols=output_columns)
+results, output_file = sc_analysis.get_moments(m)  # moment calculation
+# results, output_file = sc_analysis.apply()
+
 
 # 8. Use the SC samples and integration weights to estimate the
 #    (1-st order or all) Sobol indices. In this example, at x=1 the Sobol indices
 #    are NaN, since the variance is zero here.
 
-#get Sobol indices for free
-typ = 'first_order'
-#typ = 'all'
+# get Sobol indices for free
+#typ = 'first_order'
+typ = 'all'
 sobol_idx = sc_analysis.get_Sobol_indices(typ)
 
 my_campaign.save_state(output_json)
 ###############################################################################
 
 plt.close('all')
-from collections import OrderedDict
 
-#spatial grid of advection diffusion problem
+# spatial grid of advection diffusion problem
 x = np.linspace(0, 1, sc_analysis.N_qoi)
 
 ###################################
@@ -101,17 +101,17 @@ x = np.linspace(0, 1, sc_analysis.N_qoi)
 ###################################
 
 fig = plt.figure(figsize=[10, 5])
-ax = fig.add_subplot(121, xlabel='x', ylabel='u', \
+ax = fig.add_subplot(121, xlabel='x', ylabel='u',
                      title=r'code samples and stats')
 ax.plot(x, results['mean_f'], 'b', label='mean')
 ax.plot(x, results['mean_f'] + results['var_f']**0.5, '--r', label='std-dev')
 ax.plot(x, results['mean_f'] - results['var_f']**0.5, '--r')
 
-#plot individual SC samples
+# plot individual SC samples
 for i in range(number_of_samples):
     plt.plot(x, sc_analysis.samples[i], 'g', alpha=0.1, label='samples')
-   
-#display legend, remove duplicate entries
+
+# display legend, remove duplicate entries
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = OrderedDict(zip(labels, handles))
 leg = plt.legend(by_label.values(), by_label.keys())
@@ -121,15 +121,16 @@ leg.set_draggable(True)
 # Plot the random surrogate samples #
 #####################################
 
-ax = fig.add_subplot(122, xlabel='x', ylabel='u', \
+ax = fig.add_subplot(122, xlabel='x', ylabel='u',
                      title='some Monte Carlo surrogate samples')
 
-#generate random samples of unobserved parameter values
-left_bound = -1.0; right_bound = 1.0
+# generate random samples of unobserved parameter values
+left_bound = -1.0
+right_bound = 1.0
 n_mc = 100
-xi_mc = np.random.rand(n_mc, 2)*(right_bound - left_bound) + left_bound
+xi_mc = np.random.rand(n_mc, 2) * (right_bound - left_bound) + left_bound
 
-#evaluate the surrogate at these values
+# evaluate the surrogate at these values
 for i in range(n_mc):
     ax.plot(x, sc_analysis.surrogate(xi_mc[i]), 'g')
 
@@ -140,13 +141,21 @@ plt.tight_layout()
 ######################
 
 fig = plt.figure()
-ax = fig.add_subplot(111, xlabel='x', ylabel='Sobol indices', \
-                     title='spatial dist. Sobol indices, Pe only important in viscous regions')
-ax.plot(x, sobol_idx[0], 'b', label=r'Pe')
-ax.plot(x, sobol_idx[1], '--r', label=r'f')
+ax = fig.add_subplot(
+    111,
+    xlabel='x',
+    ylabel='Sobol indices',
+    title='spatial dist. Sobol indices, Pe only important in viscous regions')
+
+lbl = ['Pe', 'f', 'Pe-f interaction']
+idx = 0
+
+for S_i in sobol_idx:
+    ax.plot(x, S_i, label=lbl[idx])
+    idx += 1
 
 leg = plt.legend(loc=0)
-leg.draggable(True) 
+leg.draggable(True)
 
 plt.tight_layout()
 
